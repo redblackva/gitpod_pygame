3df0d90044c3276ee19fe354b7e1f0d988c65766
redblackva <21judah.hurr@utcportsmouth.org>
2024-10-21 10:57:49 +0000

    OOP 1 Girl Scouts

--- the_game.py
+++ the_game.py
@@ -1,63 +1,89 @@
 import pygame, sys
 from pygame.locals import *
 
-# set up pygame
 pygame.init()
 
-# set up the window
-windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-pygame.display.set_caption('Hello world!')
-
-# set up the colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+LIGHTRED = (255, 150, 150)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+PINK = (200, 0, 100)
+PEACH = (255, 200, 150)
 
-# set up fonts
-basicFont = pygame.font.SysFont(None, 48)
+class environment:
+    def __init__(self, dimensions, colour, caption, font):
+        self.dimensions = dimensions
+        self.colour = colour
+        self.caption = caption
+        self.font = font
+        self.window = pygame.display.set_mode((dimensions[0], dimensions[1]))
+        self.window.fill(colour)
+        pygame.display.set_caption(caption)
+    
+    def updateColour(self, newColour):
+        self.colour = newColour
+        self.window.fill(self.colour)
 
-# set up the text
-text = basicFont.render('Hello world!', True, WHITE, BLUE)
-textRect = text.get_rect()
-textRect.centerx = windowSurface.get_rect().centerx
-textRect.centery = windowSurface.get_rect().centery
+window = environment((500, 800), PEACH, "To Feed or not to Feed", pygame.font.Font('freesansbold.ttf', 32))
 
-# draw the white background onto the surface
-windowSurface.fill(WHITE)
+class button:
+    def __init__ (self, size, position, colours, text, function, surface):
+        self.size = size 
+        self.position = position
+        self.colours = colours 
+        self.function = function
+        self.surface = surface
+        self.text = surface.font.render(text, True, colours[1], colours[2])
+        self.textRect = self.text.get_rect()
+        self.textRect.center = (surface.dimensions[0] * position[0], surface.dimensions[1] * position[1])
+        self.buttonRect = pygame.Rect(surface.dimensions[0] * position[0] - size[0] / 2, surface.dimensions[1] * position[1] - size[1] / 2, size[0], size[1])
+    
+    def place(self):
+        pygame.draw.rect(self.surface.window, self.colours[0], self.buttonRect)
+        self.surface.window.blit(self.text, self.textRect)
 
-# draw a green polygon onto the surface
-pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291, 106), (236, 277), (56, 277), (0, 106)))
 
-# draw some blue lines onto the surface
-pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-pygame.draw.line(windowSurface, BLUE, (60, 120), (120, 120), 4)
+def swapF(bool):
+    if bool:
+        return False
+    else:
+        return True
+imageButton = button((200, 60), (0.5, 0.5), (PINK, LIGHTRED, PINK), "Girl Scouts", swapF, window)
 
-# draw a blue circle onto the surface
-pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
 
-# draw a red ellipse onto the surface
-pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
+class image:
+    def __init__(self, name, scale, surface):
+        self.name = name
+        self.scale = scale
+        self.surface = surface
+        self.image = pygame.image.load(name).convert()
+        self.image = pygame.transform.scale(self.image, (self.surface.dimensions[0] *self.scale[0], self.surface.dimensions[1] * self.scale[1]))
+    
+    def place(self, pos):
+        self.surface.window.blit(self.image, ((pos[0] - self.scale[0] / 2) * self.surface.dimensions[0], (pos[1] - self.scale[1] / 2) * self.surface.dimensions[1]))
 
-# draw the text's background rectangle onto the surface
-pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
+toucan1 = image("TOUCAN.jpeg", (0.4, 0.25), window)
 
-# get a pixel array of the surface
-pixArray = pygame.PixelArray(windowSurface)
-pixArray[480][380] = BLACK
-del pixArray
+BIRD = False
 
-# draw the text onto the surface
-windowSurface.blit(text, textRect)
-
-# draw the window onto the screen
-pygame.display.update()
-
-# run the game loop
 while True:
+    mouse = pygame.mouse.get_pos()
+    window.updateColour(PEACH)
+    imageButton.place()
+
+    if BIRD:
+        toucan1.place((1/2, 1/6))
+        toucan1.place((1/2, 5/6))
+
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
+            quit()
+
+        pygame.display.update()
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            if window.dimensions[0] * imageButton.position[0] - imageButton.size[0] / 2 <= mouse[0] <= window.dimensions[0] * imageButton.position[0] + imageButton.size[0] / 2:
+                if window.dimensions[1] * imageButton.position[1] - imageButton.size[1] / 2 <= mouse[1] <= window.dimensions[1] * imageButton.position[1] + imageButton.size[1] / 2:
+                    BIRD = imageButton.function(BIRD)
